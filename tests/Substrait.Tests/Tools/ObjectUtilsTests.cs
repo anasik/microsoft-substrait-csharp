e1b8fbac1fc50c4c68d8486d12a941b4ea5c2053
YongChul Kwon <yongchulkwon@microsoft.com>
2026-08-19 22:23:56 -0700

    Add ObjectUtils coverage

---
+++ tests/Substrait.Tests/Tools/ObjectUtilsTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Substrait.Tools;
+
+namespace Substrait.Tests.Tools;
+
+[TestClass]
+public sealed class ObjectUtilsTests
+{
+    private static readonly int[] Values = [1, 2, 3];
+    private static readonly int[] ReorderedValues = [3, 2, 1];
+
+    [TestMethod]
+    public void CombineHashCodesIsDeterministicAndOrderSensitive()
+    {
+        int hash = Values.CombineHashCodes();
+
+        Assert.AreEqual(hash, Values.CombineHashCodes());
+        Assert.AreNotEqual(hash, ReorderedValues.CombineHashCodes());
+    }
+}
