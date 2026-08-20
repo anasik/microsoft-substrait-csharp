59639e5c9fab261fe30f15cb8c7f8868c2555a9e
YongChul Kwon <yongchulkwon@microsoft.com>
2026-08-19 23:05:56 -0700

    Add package consumer smoke test

---
+++ tests/PackageSmokeTest/Program.cs
@@ -0,0 +1,22 @@
+using Google.Protobuf;
+using Substrait.Core.Type;
+using Substrait.Protobuf;
+
+Substrait.Protobuf.Type protoType = new()
+{
+    I64 = new Substrait.Protobuf.Type.Types.I64
+    {
+        Nullability = Substrait.Protobuf.Type.Types.Nullability.Required,
+    },
+};
+
+byte[] serialized = protoType.ToByteArray();
+Substrait.Protobuf.Type parsed = Substrait.Protobuf.Type.Parser.ParseFrom(serialized);
+
+if (parsed.KindCase != Substrait.Protobuf.Type.KindOneofCase.I64 ||
+    TypeFactory.REQUIRED.I64.Nullable != IType.NullableType.Required)
+{
+    return 1;
+}
+
+return 0;
